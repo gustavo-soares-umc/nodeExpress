4e07bce2bc55640cd6348434ea9a3b3b59d5ef7e
gustavo-soares-umc <11222101537@alunos.umc.br>
2026-04-08 20:39:03 -0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,46 +1,30 @@
+function mostrarRanking() {
+  
+    const selecoes = [
+        { nome: "França", pontos: 1877.32 },
+        { nome: "Espanha", pontos: 1876.40 },
+        { nome: "Argentina", pontos: 1874.81 },
+        { nome: "Inglaterra", pontos: 1825.97 },
+        { nome: "Portugal", pontos: 1763.83 },
+        { nome: "Brasil", pontos: 1761.16 },
+        { nome: "Holanda", pontos: 1757.87 },
+        { nome: "Bélgica", pontos: 1734.71 },
+        { nome: "Alemanha", pontos: 1730.37 },
+        { nome: "Croácia", pontos: 1717.07 }
+    ];
 
-const dadosFifa = [
-    { nome: "França", pontos: 1877.32 },
-    { nome: "Espanha", pontos: 1876.40 },
-    { nome: "Argentina", pontos: 1874.81 },
-    { nome: "Inglaterra", pontos: 1825.97 },
-    { nome: "Portugal", pontos: 1763.83 },
-    { nome: "Brasil", pontos: 1761.16 },
-    { nome: "Holanda", pontos: 1757.87 },
-    { nome: "Bélgica", pontos: 1734.71 },
-    { nome: "Alemanha", pontos: 1730.37 },
-    { nome: "Croácia", pontos: 1717.07 }
-];
-
-const rankingOrdenado = dadosFifa.sort((a, b) => b.pontos - a.pontos);
-
-function renderizarBotoes() {
-    const containerBotoes = document.getElementById("botoes-ranking");
     
-    
-    rankingOrdenado.forEach((selecao, index) => {
-        const btn = document.createElement("button");
-        btn.innerHTML = `<strong>${index + 1}º</strong> ${selecao.nome}`;
-        btn.style.margin = "5px";
-        btn.onclick = () => mostrarPlacarGeral();
-        containerBotoes.appendChild(btn);
-    });
-}
+    selecoes.sort((a, b) => b.pontos - a.pontos);
 
-function mostrarPlacarGeral() {
-    let html = "<h3>Ranking Oficial FIFA - Top 10</h3><hr>";
     
-    rankingOrdenado.forEach((sel, i) => {
-        
-        const destaque = sel.nome === "Brasil" ? "color: green; font-weight: bold;" : "";
-        
-        html += `<div style="${destaque}">
-                    <strong>${i + 1}º ${sel.nome}</strong> — ${sel.pontos.toFixed(2)} pts
-                 </div>`;
+    let htmlResultado = "<h3>Ranking Oficial FIFA - Top 10</h3><ol>";
+
+    selecoes.forEach((selecao, index) => {
+        htmlResultado += `<li><strong>${selecao.nome}</strong>: ${selecao.pontos.toFixed(2)} pontos</li>`;
     });
 
-    document.getElementById("resultado").innerHTML = html;
+    htmlResultado += "</ol>";
+
+    
+    document.getElementById("resultado").innerHTML = htmlResultado;
 }
-
-
-renderizarBotoes();
